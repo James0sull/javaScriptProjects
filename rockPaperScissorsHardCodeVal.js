ed175358cada7ccfce3bc8e2c97c8c64071f5e96
James0sull <145473944+James0sull@users.noreply.github.com>
2025-04-14 19:09:29 +0100

    Simple rock paper scissors
    The values in this are hard coded in to the programe as it was my first js programe to create and learn from as I made different versions of it as my skills got more better.

---
+++ rockPaperScissorsHardCodeVal.js
@@ -0,0 +1,64 @@
+const getUserChoice = (userInput) => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') {
+    return userInput;
+  } else {
+    console.log('Error! Please enter rock, paper, or scissors.');
+    return null; // Return null for invalid input
+  }
+};
+
+const getComputerChoice = () => {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+  if (userChoice === 'bomb') {
+    return 'User won with the bomb cheat!';
+  }
+
+  if (userChoice === computerChoice) {
+    return 'The game is a tie!';
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'Computer won!';
+    } else {
+      return 'User won!';
+    }
+  }
+  if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'Computer won!';
+    } else {
+      return 'User won!';
+    }
+  }
+  if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'Computer won!';
+    } else {
+      return 'User won!';
+    }
+  }
+};
+
+const playGame = () => {
+  const userChoice = getUserChoice('rock'); // Intentionally no argument to test undefined check.
+  const computerChoice = getComputerChoice();
+  console.log('User choice: ' + userChoice);
+  console.log('Computer choice: ' + computerChoice);
+  if (userChoice) { // only run determineWinner if userChoice is valid.
+    console.log(determineWinner(userChoice, computerChoice));
+  }
+};
+
+playGame();
